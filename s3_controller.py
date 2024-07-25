657e4349fb16b140d9f77cdb81f4bfbffe126ed7
yamenkas <yamen.kassam@ww.com>
2024-07-26 00:22:01 +0400

    presigned_url_generation

--- s3_controller.py
+++ s3_controller.py
@@ -1,6 +1,9 @@
 import boto3
 from dotenv import load_dotenv
 import os
+import logging
+from botocore.exceptions import ClientError
+
 
 load_dotenv()
 
@@ -23,8 +26,16 @@ class s3Controller():
             with open(file_path, "rb") as file:
                 self.s3.upload_fileobj(file, self.bucket_name, file_key)
             s3_url = f"https://{self.bucket_name}.s3.amazonaws.com/{file_key}"
-            return s3_url
+            return s3_url, file_key
         except Exception as e:
             raise Exception(f"Failed to upload image to S3: {str(e)}")
 
-        
+    def generate_presigned_url(self, bucket_name, file_key, expiration=3600):
+        try:
+            response = self.s3.generate_presigned_url('get_object',
+                                                Params={'Bucket': bucket_name, 'Key': file_key},
+                                                ExpiresIn=expiration)
+        except ClientError as e:
+            logging.error(e)
+            return None
+        return response
